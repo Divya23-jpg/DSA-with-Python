3378cbd8f2ed5d0201ff446ebd96e8cb0ebabd8a
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-09-24 23:16:38 +0530

    DSA

--- Linear Data Structure/hash_map.py
+++ Linear Data Structure/hash_map.py
@@ -52,4 +52,43 @@ def Two_sum_transaction(nums,target):
 
 nums=[7,-3,2,7,5,-3,10,0,2,8,5,-8,3,12,-3]
 target=9
-print(Two_sum_transaction(nums,target))
+# print(Two_sum_transaction(nums,target))
+
+
+
+# ! 49. Group Anagrams
+
+def groupAnagrams(strs):
+    d = {}
+    for word in strs:
+        key = ''.join(sorted(word))
+        if key in d:
+            d[key].append(word)
+        else:
+            d[key] = [word]
+    return list(d.values())
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams(strs))
+
+
+# ! 904. Fruit Into Baskets
+def totalFruit(a):
+    i = 0
+    d = {}
+    maxlen = 0
+    for j in range(len(a)):
+        if a[j] in d:
+            d[a[j]] += 1
+        else:
+            d[a[j]] = 1
+        while len(d) > 2:
+                d[a[i]] -= 1
+                if d[a[i]] == 0:
+                    del d[a[i]]
+                i += 1
+        maxlen = max(maxlen, j - i + 1)
+    return maxlen
+
+a = [1,2,1]
+print(totalFruit(a))
--- Linear Data Structure/linklist.py
+++ Linear Data Structure/linklist.py
@@ -1,40 +1,90 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self,data):
+        self.data=data 
+        self.next=None
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
 
-    def append(self, val):
-        new_node = Node(val)
-        if not self.head:
-            self.head = new_node
-            return
+    # Creating Nodes
+    node1=Node(10)
+    node2=Node(20)
+    node3=Node(30)
+    node4=Node(40)
 
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
 
-    def display(self):
-        curr = self.head  # Fixed: changed self.first to self.head
-        ele = []
-        while curr is not None:
-            ele.append(str(curr.data))  # Fixed: append data as string
-            curr = curr.next
-        print(" -> ".join(ele) + " -> None")
+
+
+    node1.next=node2
+    node2.next=node3
+    node3.next=node4
+
+
+    curr=node1
+    while curr is not None:
+        print(curr.data,end='->')
+        curr=curr.next
+
+    print("None")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def append(self, val):
+#         new_node = Node(val)
+#         if not self.head:
+#             self.head = new_node
+#             return
+
+#         current = self.head
+#         while current.next:
+#             current = current.next
+#         current.next = new_node
+
+#     def display(self):
+#         curr = self.head  # Fixed: changed self.first to self.head
+#         ele = []
+#         while curr is not None:
+#             ele.append(str(curr.data))  # Fixed: append data as string
+#             curr = curr.next
+#         print(" -> ".join(ele) + " -> None")
 
 
 # --- Method 1: Using append() ---
-ll = LinkedList()
-ll.append(10)
-ll.append(20)
-ll.append(30)
+# ll = LinkedList()
+# ll.append(10)
+# ll.append(20)
+# ll.append(30)
 
-ll.display()
+# ll.display()
 # Output: 10 -> 20 -> 30 -> None
 
 
